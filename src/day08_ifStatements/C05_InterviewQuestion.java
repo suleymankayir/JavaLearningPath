bf97499cc588ff39c07a2d86fdf8a2b257854618
suleymankayir <suleymantalhakayir@gmail.com>
2023-04-14 01:31:42 +0300

    artik yil ınterview question solved by myself

--- src/day08_ifStatements/C05_InterviewQuestion.java
+++ src/day08_ifStatements/C05_InterviewQuestion.java
@@ -6,28 +6,24 @@ public class C05_InterviewQuestion {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Lutfen artık yıl kontrolü yapılacak sayıyı giriniz.");
-        int sayi = scan.nextInt();
-        if (sayi%4 == 0){
-            if (sayi%100 != 0){
+        int yil = scan.nextInt();
+        if (yil%4 == 0){
+            if (yil%100 != 0){
                 System.out.println("Artık yildir.");
-            } else if (sayi%400 == 0){
+            } else if (yil%400 == 0){
                 System.out.println("Artık yildir");
             } else {
                 System.out.println("Artık yıl değildir.");
             }
-        /* KENDİ CEVABIM
-            if (sayi%400 == 0 && sayi%100 == 0) {
+        /* KENDI CEVABIM
+            if (yil%400 == 0 && yil%100 == 0) {
                 System.out.println("Artık yildir.");
-            } else if (sayi%100 == 0) {
+            } else if (yil%100 == 0) {
                 System.out.println("Artık yıl değildir");
             } else {
                 System.out.println("Artık yildir");
             }
         */
-
-
-
-
         } else {
             System.out.println("Artık yıl değildir.");
         }
