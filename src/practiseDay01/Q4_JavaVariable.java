8fbe603a1f939aaea830b8dddcc84324d4e62f6f
suleymankayir <suleymantalhakayir@gmail.com>
2023-04-16 20:32:44 +0300

    JavaPractiseCommit but file location changed.

--- src/practiseDay01/Q4_JavaVariable.java
+++ src/practiseDay01/Q4_JavaVariable.java
@@ -1,4 +1,14 @@
 package practiseDay01;
 
 public class Q4_JavaVariable {
+    public static void main(String[] args) {
+        int yas = 33;
+        double boy = 1.70;
+        System.out.println("yas = " + yas);
+        System.out.println("boy = " + boy);
+
+        int yil = 2022, ay=6, gun=23; // tek satirda coklu degisken tanımlama
+        System.out.println("gun = " + gun);
+
+    }
 }
