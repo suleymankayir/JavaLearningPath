6f65362e2d4b88a567df0f11ee61b3ce992b83b9
suleymankayir <suleymantalhakayir@gmail.com>
2023-04-27 00:45:29 +0300

    JavaLearningPath method creation

---
+++ src/day13_MethodCreation/Q1_MethodCreation.java
@@ -0,0 +1,14 @@
+package day13_MethodCreation;
+
+public class Q1_MethodCreation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        topla(a,b);
+    }
+
+    public static void topla(int a, int b) {
+        System.out.println("Girilen iki sayinin toplami : " + (a+b));
+    }
+
+}
