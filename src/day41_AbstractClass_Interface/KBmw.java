03e7613de6cb3b60be6cb115bc9d972884605084
suleymankayir <suleymantalhakayir@gmail.com>
2023-06-09 10:29:31 +0300

    JavaLearningPath day41 last commit

---
+++ src/day41_AbstractClass_Interface/KBmw.java
@@ -0,0 +1,11 @@
+package day41_AbstractClass_Interface;
+
+public abstract class KBmw extends DAraba{
+    /*
+    Abstract bir class'in abstract bir child'i oldugu icin
+    Parent class'daki abstract method'lari implement etmek zorunda kalmadi
+     */
+
+    public abstract void amblem();
+    public abstract void guvenlik();
+}
---
+++ src/day41_AbstractClass_Interface/LX3.java
@@ -0,0 +1,33 @@
+package day41_AbstractClass_Interface;
+
+public class LX3 extends KBmw{
+
+    /*
+    X3 Class'i abstract silsilesinden sonra gelen ilk concrete class oldugundan tum abstract
+    parent'lardaki, tum abstract method'lari override etmek zorunda kaldi.
+     */
+    @Override
+    protected void yakit() {
+
+    }
+
+    @Override
+    protected void kaporta() {
+
+    }
+
+    @Override
+    protected void motor() {
+
+    }
+
+    @Override
+    public void amblem() {
+
+    }
+
+    @Override
+    public void guvenlik() {
+
+    }
+}
